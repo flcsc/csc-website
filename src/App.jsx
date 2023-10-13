bc0435b9af18dbc6df1891f6dbdaa73f202b9632
Hanz Po <hanznathanpo@gmail.com>
2023-10-12 22:17:09 -0400

    Migrate website to React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,23 @@
+function Projects() {
+  return (
+    <>
+      <header>
+        <div>
+          <a href="/">
+            <img class="home-button" src="logo1.png" />
+          </a>
+          <a href="https://tcdsb.elearningontario.ca/d2l/home/23924060">
+            D2L Page
+          </a>
+          <a class="current-page" href="/projects">
+            Projects
+          </a>
+        </div>
+      </header>
+      <div class="main"></div>
+      <footer></footer>
+    </>
+  );
+}
+
+export default Projects;
