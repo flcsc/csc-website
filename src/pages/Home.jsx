b4f4d462301aef9af6cd16b72513208036e47f62
PINTOGavin <pintog001@tcdsb.ca>
2023-10-17 12:28:43 -0400

    d2l button will open new tab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
       <header>
         <div>
           <a href="/"><img className="home-button" src="logo5.png" /></a>
-          <a href="https://tcdsb.elearningontario.ca/d2l/home/23924060">D2L Page</a>
+          <a href="https://tcdsb.elearningontario.ca/d2l/home/23924060" target="blank">D2L Page</a>
           <a href="/projects">Projects</a>
         </div>
       </header>
