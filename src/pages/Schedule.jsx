0ad56de76f75d64464bab080cccd63d3af00be20
Noah Perez <noahm.p.in2017@gmail.com>
2023-10-23 14:04:47 -0400

    Started adding state to the Schedule page

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,31 +1,37 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Event from "../components/Event"
-import "../data/events.json"
+import React from "react"
 
-function getSchedule()
-{
-    let events = fetch("/src/data/events.json").then((result) => result.json()).finally((res)=> res);
-    console.log(events);
-}
+class Schedule extends React.Component{
+    events = [];
 
-function Schedule()
-{
-    getSchedule();
-    var events = [
-        <Event name="NewHacks" description="A hackathon hosted at the University of Toronto St. George campus, which will last 24-hours. Registration will close on October 29th, and there will be limited spots!" date="Nov 4-5"/>,
-        <Event name="SkillsCanada" description="A coding competition for hackers to compete to increasing regional levels." />,
-        <Event name="CanHack" description="A cybersecurity hacking competition. Consists of many 'Caputure the Flag' puzzles in which competitors must find a code (the flag) hidden using different sort of encryption at different difficulties." date="Unknown"/>
-    ];
-    return (
-        <>
-            <Navbar />
-            <div className = "main">
-                {events}
-            </div>
-            <Footer />
-        </>
-    )
+    constructor(props)
+    {
+        super(props);
+
+        events = props.events;
+        fetch("/src/data/events.json")
+        .then(result => result.json()).then((data) =>
+        {
+            data.events.forEach(event => events.push(<Event name={event.name} description={event.description} date={event.date}/>))
+            console.log(events);
+            this.setState({events: this.state.events});
+        }, console.error());
+    }
+
+    render()
+    {
+        return (
+            <>
+                <Navbar />
+                <div className = "main">
+                    {events}
+                </div>
+                <Footer />
+            </>
+        );
+    }
 }
 
 export default Schedule;
