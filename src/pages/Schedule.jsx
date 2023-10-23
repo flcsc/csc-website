f315112aa4b99c47a787f8cc244f36daa002ff5d
Noah Perez <noahm.p.in2017@gmail.com>
2023-10-23 18:21:52 -0400

    Added ability to read events from a file. Minor problem, componentDidMount() seems to be getting called twice due to strict mode being turned on

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -9,24 +9,49 @@ class Schedule extends React.Component{
     constructor(props)
     {
         super(props);
+        this.state = {events:props.events};
+    }
 
-        events = props.events;
+    componentDidMount()
+    {
+        if(this.state.events.length !== 0) return; 
+        console.log("Component did mount. Length: " + this.state.events.length)
+
         fetch("/src/data/events.json")
-        .then(result => result.json()).then((data) =>
+        .then(result => result.json(), console.error())
+        .then((data) =>
         {
-            data.events.forEach(event => events.push(<Event name={event.name} description={event.description} date={event.date}/>))
-            console.log(events);
+            data.events.forEach(event => this.state.events.push(<Event name={event.name} description={event.description} date={event.date}/>))
             this.setState({events: this.state.events});
         }, console.error());
     }
 
+    componentWillUnmount()
+    {
+        for(let i = 0; i < this.state.events.length; i++)
+            this.state.events[i] = null;
+    }
+
     render()
     {
+        if(this.state.events.length === 0)
+        {
+            return(
+                <>
+                    <Navbar/>
+                    <div className="main">
+                        <h1>There are no scheduled events (just yet)!</h1>
+                    </div>
+                    <Footer/>
+                </>
+            )
+        }
+
         return (
             <>
                 <Navbar />
                 <div className = "main">
-                    {events}
+                    {this.state.events}
                 </div>
                 <Footer />
             </>
